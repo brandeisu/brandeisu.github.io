4400866547e898ae988732e47c7235ba4a8d232d
gradyberryward <gward@brandeis.edu>
2016-04-22 14:31:21 -0500

    Fixed bug in the way that editing links are created.

--- static/js/init.js
+++ static/js/init.js
@@ -7,8 +7,24 @@
     
     var editorURL = "http://githubdemo-1285.appspot.com/edit";
     
-    var pathToPage = window.location.href;
-	pathToPage = pathToPage.substring(pathToPage.indexOf("/", 8)+1);
+    var pathToPage = window.location.pathname;
+    
+    if (pathToPage.contains("index.html"){
+    	pathToPage = pathToPage.replace("index.html","");
+    }
+    
+    if (pathToPage.contains("website")){
+    	pathToPage = pathToPage.replace("website", "");
+    }
+    
+    if (pathToPage.length > 0 && pathToPage[pathToPage.length - 1] == '/'){
+    	pathToPage = pathToPage.substring(0, pathToPage.length - 1);
+    }
+    
+    if (pathToPage.length > 0 && pathToPage[0] == '/'){
+    	pathToPage = pathToPage.substring(1);
+    }
+    
 	$("#edit-now-link").attr("href", editorURL + "/" + pathToPage);
 		
   });
